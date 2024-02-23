5691a87b6b2647111295ebd830dbfdfe45f40b1f
Dan Fears (Microsoft) <danielfears@microsoft.com>
2024-02-23 00:10:14 +0000

    altered to work foreach of a set of child management groups - going to readd sub functionality at a later date

--- PolicyRemediation.ps1
+++ PolicyRemediation.ps1
@@ -1,21 +1,21 @@
-function Start-PolicyInitiativeRemediation {
+function Start-PolicyInitiativeRemediationForMG {
     param (
-        [string]$SubscriptionId,
+        [string]$ManagementGroupId,
         [string[]]$PolicyInitiativeAssignmentNames,
-        [string]$PolicyInitiativeDefinitionName
+        [string]$PolicyInitiativeDefinitionName,
+        [string]$parentManagementGroupName,
+        [string]$ManagementGroupName # New parameter to pass the name of the Management Group
     )
 
-    # Select the subscription
-    Select-AzSubscription -SubscriptionId $SubscriptionId > $null
-    Write-Host "Selected subscription: $SubscriptionId"
+    Write-Host "Processing Management Group: $ManagementGroupName ($ManagementGroupId)" # Updated to include the name
 
     # Retrieve all policy assignments
-    $PolicyAssignments = Get-AzPolicyAssignment -WarningAction SilentlyContinue | Select-Object *, 
+    $PolicyAssignments = Get-AzPolicyAssignment -Scope $ManagementGroupId -WarningAction SilentlyContinue | Select-Object *, 
     @{Name = "InitiativeDisplayName"; Expression = { $_.Properties.DisplayName } },
     @{Name = "Scope"; Expression = { $_.Properties.Scope } },
     @{Name = "PolicyDefinitionId"; Expression = { $_.Properties.PolicyDefinitionId } } -ExcludeProperty 'Properties'
 
-    $policySetDefinition = Get-AzPolicySetDefinition | Where-Object { $_.Name -eq $PolicyInitiativeDefinitionName }
+    $policySetDefinition = Get-AzPolicySetDefinition -ManagementGroupName $parentManagementGroupName | Where-Object { $_.Properties.DisplayName -eq $PolicyInitiativeDefinitionName }
     $policySetDefinition = $policySetDefinition.Properties.PolicyDefinitions
 
     foreach ($policyInitiativeAssignmentName in $PolicyInitiativeAssignmentNames) {
@@ -26,54 +26,63 @@ function Start-PolicyInitiativeRemediation {
                 Write-Host "Found policy initiative assignment with ID: $($assignment.PolicyAssignmentId) for initiative '$policyInitiativeAssignmentName'"
 
                 foreach ($policyDefinition in $policySetDefinition) {
-                    $remediationName = "Remediation-" + $($policyDefinition.PolicyDefinitionId.Split("/")[-1]) + "-" + (Get-Date -Format "yyyyMMddHHmmssfff")
-
-                    Write-Host ""
-                    Write-Host "Remediating the following:"
-                    Write-Host "Subscription: $SubscriptionId"
-                    Write-Host "Policy Initiative: $policyInitiativeAssignmentName"
-                    Write-Host "Policy Name: $($policyDefinition.PolicyDefinitionId.Split("/")[-1])"
-                    Write-Host "Policy Reference ID: $($policyDefinition.policyDefinitionReferenceId)"
-                    Write-Host "Remediation Name: $remediationName"
 
                     # Create the remediation job
-                    Start-AzPolicyRemediation -Name $remediationName -PolicyAssignmentId $assignment.PolicyAssignmentId -PolicyDefinitionReferenceId $policyDefinition.policyDefinitionReferenceId -AsJob
+                    try {
+
+                        $remediationName = "Remediation-" + $($policyDefinition.PolicyDefinitionId.Split("/")[-1]) + "-" + (Get-Date -Format "yyyyMMddHHmmssfff")
+
+                        Write-Warning "`nRemediating the following:"
+                        Write-Host "Management Group: $ManagementGroupName"
+                        Write-Host "Policy Initiative: $policyInitiativeAssignmentName"
+                        Write-Host "Policy Name: $($policyDefinition.PolicyDefinitionId.Split("/")[-1])"
+                        Write-Host "Policy Reference ID: $($policyDefinition.policyDefinitionReferenceId)"
+                        Write-Host "Remediation Name: $remediationName"
+
+                        Start-AzPolicyRemediation -Scope $ManagementGroupId -Name $remediationName -PolicyAssignmentId $assignment.PolicyAssignmentId -PolicyDefinitionReferenceId $policyDefinition.policyDefinitionReferenceId -AsJob
+
+                    }
+                    catch {
+
+                        Write-Error "Error occurred while remediating the following:"
+                        Write-Host "Management Group: $ManagementGroupName"
+                        Write-Host "Policy Initiative: $policyInitiativeAssignmentName"
+                        Write-Host "Policy Name: $($policyDefinition.PolicyDefinitionId.Split("/")[-1])"
+                        Write-Host "Policy Reference ID: $($policyDefinition.policyDefinitionReferenceId)"
+                        Write-Host "Remediation Name: $remediationName"
+
+                        Write-Host "Error: $_"
+                    }
+
+                    Start-Sleep -Milliseconds 100
                 }
             }
         } else {
-            Write-Warning "No policy assignment found for initiative '$policyInitiativeAssignmentName' in Subscription '$SubscriptionId'."
+            Write-Warning "No policy assignment found for initiative '$policyInitiativeAssignmentName' in Management Group '$ManagementGroupId'."
         }
     }
 }
 
-# Define the names of any policy initiatives to remediate
-$policyInitiativeAssignmentNames = @("policyInitiativeAssignmentName1", "policyInitiativeAssignmentName2")
-$policyInitiativeDefinitionName = "policyInitiativeDefinitionName"
-
-# Set this to only run against a single Subscription - LEAVE BLANK TO ITERATE THROUGH ALL SUBSCRIPTIONS IN A GIVEN TENANT
-$subscriptionId = ""
-
-# Check for existing Azure session
-$context = Get-AzContext -ErrorAction SilentlyContinue
-if ($null -eq $context) {
+# Check if already logged in to Azure
+$currentAzureContext = Get-AzContext
+if (-not $currentAzureContext) {
+    Write-Host "Not logged in to Azure. Initiating login..."
     Connect-AzAccount
 } else {
-    Write-Host "Already connected to Azure as $($context.Account)"
+    Write-Host "Already logged in as $($currentAzureContext.Account)"
 }
 
-# Determine whether to process a single subscription or all subscriptions
-if (-not [string]::IsNullOrWhiteSpace($subscriptionId)) {
-    Start-PolicyInitiativeRemediation -SubscriptionId $subscriptionId -PolicyInitiativeAssignmentNames $policyInitiativeAssignmentNames -PolicyInitiativeDefinitionName $policyInitiativeDefinitionName
-} else {
-    $subscriptions = Get-AzSubscription
-    if ($null -eq $subscriptions -or $subscriptions.Count -eq 0) {
-        Write-Host "No subscriptions found. Please check your Azure account permissions."
-    } else {
-        foreach ($subscription in $subscriptions) {
-            Start-PolicyInitiativeRemediation -SubscriptionId $subscription.Id -PolicyInitiativeAssignmentNames $policyInitiativeAssignmentNames -PolicyInitiativeDefinitionName $policyInitiativeDefinitionName
-        }
-    }
+# Fetching child Management Groups
+$parentManagementGroupName = "YourParentManagementGroupName"
+$childManagementGroups = Get-AzManagementGroup -GroupName $parentManagementGroupName -Recurse -Expand
+
+# Define the policy initiative definition name and assignment names
+$PolicyInitiativeDefinitionName = "YourPolicyInitiativeDefinitionName"
+$PolicyInitiativeAssignmentNames = @("YourPolicyInitiativeAssignmentName1", "YourPolicyInitiativeAssignmentName2")
+
+# Loop through each child Management Group for policy remediation
+foreach ($mg in $childManagementGroups.Children) {
+    Start-PolicyInitiativeRemediationForMG -ManagementGroupId $mg.Id -ManagementGroupName $mg.DisplayName -parentManagementGroupName $parentManagementGroupName -PolicyInitiativeAssignmentNames $PolicyInitiativeAssignmentNames -PolicyInitiativeDefinitionName $PolicyInitiativeDefinitionName
 }
 
-Write-Host ""
-Write-Warning "Script completed."
+Write-Host "`nAll specified policy initiative remediations have been initiated for child management groups under '$parentManagementGroupName'."
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Azure Policy Initiative Remediation Script
 
-This PowerShell script has been designed to automate the remediation of Azure policy initiatives, with the flexibility to target a single subscription or iterate across multiple subscriptions within an Azure tenant. It checks for policy initiative assignments and initiates remediation processes where applicable, leveraging a reusable function for efficient background execution.
+This PowerShell script automates the remediation of Azure policy initiatives, now offering expanded functionality to operate not just across subscriptions but also within Azure Management Groups. It locates policy initiative assignments and triggers remediation processes where necessary, employing a reusable function for efficient execution, both at the subscription level and across management group hierarchies.
 
 ## Prerequisites
 
@@ -8,35 +8,34 @@ Before executing this script, ensure that you have:
 
 - PowerShell installed on your system.
 - The Azure PowerShell module (`Az` module) installed. Install it using the command: `Install-Module -Name Az -AllowClobber -Scope CurrentUser`.
-- Necessary permissions to manage Azure Policy assignments and remediations within your Azure tenant.
+- Necessary permissions to manage Azure Policy assignments and remediations within your Azure tenant, including the management groups.
 
 ## Configuration
 
-1. **Set Policy Initiatives**:
-   Modify the `$policyInitiativeAssignmentNames` array and `$policyInitiativeDefinitionName` variable at the beginning of the script to include the policy initiatives you aim to remediate.
+1. **Management Group and Policy Initiatives Configuration**:
+   The script now requires specifying the target management group names and policy initiative details. Adjust the `$parentManagementGroupName`, `$PolicyInitiativeDefinitionName`, and `$PolicyInitiativeAssignmentNames` variables at the start of the script.
 
     ```powershell
-    $policyInitiativeAssignmentNames = @(
-        "policyInitiativeAssignmentName1",
-        "policyInitiativeAssignmentName2"
-    )
-    $policyInitiativeDefinitionName = "policyInitiativeDefinitionName"
+    $parentManagementGroupName = "YourParentManagementGroupName"
+    $PolicyInitiativeDefinitionName = "YourPolicyInitiativeDefinitionName"
+    $PolicyInitiativeAssignmentNames = @("YourPolicyInitiativeAssignmentName1", "YourPolicyInitiativeAssignmentName2")
     ```
 
-2. **Specify Subscription ID** (Optional):
-   To target a specific subscription, set the `$subscriptionId` variable. Leave it blank (`""`) to iterate through all subscriptions in the tenant.
+2. **Management Group Targeting**:
+   The script has been enhanced to support iterating through child management groups under a specified parent management group for policy remediation. This is in addition to the optional subscription targeting.
 
     ```powershell
-    $subscriptionId = "<Your-Subscription-ID>"
+    # Fetching child Management Groups
+    $childManagementGroups = Get-AzManagementGroup -GroupName $parentManagementGroupName -Recurse -Expand
     ```
 
 ## Execution
 
 - **Authenticate with Azure**:
-  If not already authenticated, the script will prompt you for your Azure credentials when run.
+  If not already authenticated, the script will prompt you for your Azure credentials upon execution.
 
 - **Run the Script**:
-  Navigate to the directory containing the script and execute it by running:
+  Navigate to the script's directory and execute it with:
 
     ```powershell
     .\PolicyRemediation.ps1
@@ -44,38 +43,22 @@ Before executing this script, ensure that you have:
 
 ## Monitoring and Managing Background Tasks
 
-The script initiates remediation jobs as background tasks. Use PowerShell job commands to manage these tasks:
+Remediation jobs initiated by the script run as background tasks. Utilize PowerShell job commands for task management:
 
-- **Check status** of all background jobs:
-
-    ```powershell
-    Get-Job
-    ```
-
-- **Receive output** from a completed job:
-
-    ```powershell
-    Receive-Job -Id <job-id>
-    ```
-
-- **Remove** a finished job:
-
-    ```powershell
-    Remove-Job -Id <job-id>
-    ```
+- **Check the status** of all background jobs with `Get-Job`.
+- **Receive output** from a completed job using `Receive-Job -Id <job-id>`.
+- **Remove** a finished job with `Remove-Job -Id <job-id>`.
 
 ## Output
 
-The script outputs each step of the remediation process to the PowerShell console, detailing found policy assignments and the initiation of remediation jobs.
+The script provides detailed console output for each step of the remediation process, including identification of policy assignments and the launch of remediation jobs.
 
 ## Important Notes
 
-- The script is configured to automatically start the remediation process. Ensure you have reviewed and are comfortable with the actions it will take.
-
-- Verify the correct setup of policy initiatives and definitions in your Azure environment prior to running the script.
-
-- Adequate permissions are required to view and manage policy assignments and remediations across your Azure tenant's subscriptions.
+- The script now includes management group handling for comprehensive policy initiative remediation across your Azure environment.
+- Ensure that policy initiatives and definitions are correctly configured in your Azure environment, including the necessary hierarchy of management groups, prior to execution.
+- Adequate permissions are needed to view and manage policy assignments and remediations across subscriptions and management groups within your tenant.
 
 ## Disclaimer
 
-This script is provided "as-is". It is recommended to test in a non-production environment before using it in production settings.
+This script is provided "as-is", with recommendations to test in a non-production environment before implementation in production settings.
